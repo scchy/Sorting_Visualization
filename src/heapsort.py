457f9d97a58d2c6b68b176ae85bb01d39bf904ac
Scc_hy <34824890+scchy@users.noreply.github.com>
2019-07-29 23:23:46 +0800

    heapsort_updata.py
    修改堆排序

--- src/heapsort.py
+++ src/heapsort.py
@@ -1,64 +1,52 @@
-from .data import DataSeq
+#！ python 3.6
+# Create Date: 2019-07-29
+# Author: Scc_hy
+# Function: 堆排序 HeapSort
+# 加载包
+from .data import get_data
+from .data import get_figure2draw
 
-def CheckMaxHeap(data, size, child):
-    if child <= size:
-        father = child//2
-        if data[child]>data[father]:
-            print("error found")
-        CheckMaxHeap(data, size, child*2)
-        CheckMaxHeap(data, size, child*2+1)
 
+def big_endian(ds, root, end):
+    """
+    将堆的末端子节点作调整，使得子节点永远小于父节点  
+    :param: ds    get_figure2draw  
+    :param: root int 开始（list index）  
+    :param: end int 结束（list index）  
+    """
+    arr = ds.data
+    child = root * 2 +  1 # 左子节点
+    while child <= end: 
+        if child + 1 <= end and arr[child] < arr[child + 1] :
+        # 判断右child是否存在，如果存在则和另外一个同级节点进行比较
+            child += 1
+        if arr[root] < arr[child]:
+            ds.swap(root, child)
+            ## 下钻到下层
+            root = child
+            child = root * 2 + 1
+        else:
+            break
 
 def HeapSort(ds):
-    assert isinstance(ds, DataSeq), "Type Error"
-
-    Length = ds.length
-    # 先构建最大堆
-    for i in range(1,Length):
-        X = ds.data[i]
-        child = i
-        father = (child+1)//2-1
-        while X>ds.data[father] and father>=0:
-            ds.SetVal(child, ds.data[father])
-            ds.Visualize((child,father))
-            child=father
-            father = (child+1)//2-1
-        ds.SetVal(child, X)
-        ds.Visualize((child,))
-
-    # 检查最大堆是否正确
-    # data = [100000]+ds.data
-    # print(data)
-    # CheckMaxHeap(data, ds.length, 1)
-
-    # 再反向弹出
-    p=ds.length-1
-    while(p>0):
-        maxval = ds.data[0]
-        last = ds.data[p]
-        father = 0
-        child = (father+1)*2-1
-        while child<p:
-            if child!=(p-1) and ds.data[child]<ds.data[child+1]:
-                child += 1
-            if ds.data[child]<last:
-                break
-            else:
-                ds.SetVal(father, ds.data[child])
-                ds.Visualize((father, child))
-                father = child
-                child = (father+1)*2-1
-        ds.SetVal(father, last)
-        ds.SetVal(p,maxval)
-        ds.Visualize((p,))
-        p-=1
+    """
+    堆排序
+    """
+    Length = ds.length - 1
+    first = Length // 2 
+    for root in range(first, -1, -1):
+    # 建堆
+        big_endian(ds, root, Length)
+    for end in range(Length, 0, -1):
+        # 堆顶是最大的值，放到最末尾，长度-1后继续建堆
+        ds.swap(0, end) 
+        big_endian(ds, 0, end - 1) 
 
 
 if __name__ == "__main__":
-    ds=DataSeq(64)
-    ds.Visualize()
-    ds.StartTimer()
+    data = get_data(64)
+    ds = get_figure2draw(data, sort_title = 'HeapSort')
+    ds.Visualize() # 画出底图
     HeapSort(ds)
-    ds.StopTimer()
-    ds.SetTimeInterval(0)
-    ds.Visualize()
+    ds.set_time_interval(0)
+    ds.Visualize() # 画出排序结束后的图
