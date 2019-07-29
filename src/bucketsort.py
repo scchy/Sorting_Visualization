ef529d14aa7bfe3609abb2c3d4c5b4eed5242ac5
Scc_hy <34824890+scchy@users.noreply.github.com>
2019-07-29 17:40:35 +0800

    BucketSort_update.py
    修改为 空桶[ 待排数组[ 0 ] ] = 待排数组[ 0 ] 
    再从空桶中拿值

--- src/bucketsort.py
+++ src/bucketsort.py
@@ -2,24 +2,28 @@ import copy
 
 from .data import DataSeq
 
+
 def BucketSort(ds):
     """
-        桶排序只适用于整数排序，且最大元素不能比数组元素大太多的情况
+    桶排序只适用于整数排序，且最大元素不能比数组元素大太多的情况  
+    空桶[ 待排数组[ 0 ] ] = 待排数组[ 0 ]
     """
     assert isinstance(ds, DataSeq), "Type Error"
     assert isinstance(ds.data[0], int), "Type Error"
 
     Length = ds.length
-    bucket = [0 for _ in range(Length)]
+    dt = ds.data
+    bucket = [0 for _ in range(max(dt) + 1)]
     for i in range(Length):
-        bucket[ds.data[i]] += 1
-    j=0
+        bucket[ dt[i] ] = dt[i] 
+
+    j = 0
     for i in range(Length):
-        tmp = bucket[i]
-        while tmp>0:
-            ds.SetVal(j, i)
-            tmp-=1
-            j+=1
+        tmp = bucket[i] 
+        while tmp != 'stp': # 有值位置才排序
+            ds.SetVal(j , tmp)
+            tmp = 'stp'
+            j += 1
 
 
 if __name__ == "__main__":
