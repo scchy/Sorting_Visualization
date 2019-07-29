1db0a5c7d3a2012e5f4748720c326a138d8df955
Scc_hy <34824890+scchy@users.noreply.github.com>
2019-07-29 17:49:00 +0800

    Bubblesort_update.py

--- src/bubblesort.py
+++ src/bubblesort.py
@@ -1,19 +1,31 @@
-from .data import DataSeq
+#！ python 3.6
+# Create Date: 2019-06-19
+# Author: Scc_hy 
+# Function: 冒泡排序 Bubblesort
 
-def BubbleSort(ds):
-    assert isinstance(ds, DataSeq), "Type Error"
+from .data import get_data
+from .data import get_figure2draw
 
-    Length = ds.length
-    for i in range(Length-1, -1, -1):
-        for j in range(0, i, 1):
-            if ds.data[j] > ds.data[j+1]:
-                ds.Swap(j, j+1)
+
+def Bubblesort(ds):
+    """
+    气泡排序
+    """
+    # isinstance(object, classinfo) 可以是直接或间接类名
+    assert isinstance(ds, get_figure2draw), "Type Error"
+
+    Length = ds.length  
+    dt = ds.data
+    for i in range(Length-1, -1, -1): # 倒序
+        for j in range(0, i, 1): # 顺序
+            if dt[j] > dt[j+1]: # 逐一比较相邻两个值的大小
+                ds.swap(j, j+1) # 交换 idx1, idx2 的值
+
 
 if __name__ == "__main__":
-    ds=DataSeq(64)
-    ds.Visualize()
-    ds.StartTimer()
-    BubbleSort(ds)
-    ds.StopTimer()
-    ds.SetTimeInterval(0)
-    ds.Visualize()
+    data = get_data(64)
+    ds = get_figure2draw(data, sort_title = 'Bubblesort')
+    ds.Visualize() # 画出底图
+    Bubblesort(ds)
+    ds.set_time_interval(0)
+    ds.Visualize() # 画出排序结束后的图
